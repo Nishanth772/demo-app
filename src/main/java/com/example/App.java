545712a29b7a2d5d8662c3850ba4e85370de8757
Nishanth772 <nishanththulasidhar@gmail.com>
2026-09-18 14:42:18 +0530

    Test the automatic build trigger

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, CI/CD Pipeline!");
         System.out.println("Result: " + add(5, 7));
-	System.out.println("Automatic build trigger successful");
+	System.out.println("Automatic build triggered succeessfully");
     }
 
     public static int add(int a, int b) {
