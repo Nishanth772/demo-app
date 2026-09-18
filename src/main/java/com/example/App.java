878209996da507640bc4c0cb5f89b3160b864cba
Nishanth772 <nishanththulasidhar@gmail.com>
2026-09-18 11:46:31 +0530

    Initial commit - demo-app

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, CI/CD Pipeline!");
+        System.out.println("Result: " + add(5, 7));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
